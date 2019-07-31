f918e6fc38bc120913a5a834cfa2b3c5be1d37e9
Emre Sert <emresert1233@gmail.com>
2019-07-31 14:14:42 +0000

    Basic List Report for Northwind Data Service is completed.

---
+++ BasicListReportApp/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("BasicListApp.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
