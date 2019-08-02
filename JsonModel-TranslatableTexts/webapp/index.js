b822a0401c7c2ff75f7ae4b1dd7c3df5262c5fea
Emre Sert <emresert1233@gmail.com>
2019-08-02 12:26:45 +0000

    Code usage comments is added.

--- JsonModel-TranslatableTexts/webapp/index.js
+++ JsonModel-TranslatableTexts/webapp/index.js
@@ -1,10 +1,12 @@
+// index.js should be added by manuel  
+
 sap.ui.define([
 	"sap/ui/core/mvc/XMLView"
 ], function (XMLView) {
 	"use strict";
 
 	XMLView.create({
-		viewName: "JsonModel-TranslatableTexts.view.App"
+		viewName: "JsonModel-TranslatableTexts.view.App" // it should has a correct url to work properly
 	}).then(function (oView) {
 		oView.placeAt("content");
 	});
