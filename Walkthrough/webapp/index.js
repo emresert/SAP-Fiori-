520d608a6c442d8d3264110bfdca7fc8cd13ec6b
Emre Sert <emresert1233@gmail.com>
2019-08-06 07:57:58 +0000

    Walkthrough samples folder is added.

---
+++ Walkthrough/webapp/index.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
+	"use strict";
+
+	new ComponentContainer({
+		name: "Walkthrough",
+		settings : {
+			id : "Walkthrough" 
+		},
+		async: true
+	}).placeAt("content");
+});
